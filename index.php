3cb2d70fd77919f7b0613abfc0e40bd2fb78b449
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-12 14:17:06 +0100

    update index.php

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 
 echo '<h1>PHP Form</h1>'; 
 
-
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="female") echo "checked";?>
+value="female">Female
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="male") echo "checked";?>
+value="male">Male
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="other") echo "checked";?>
+value="other">Other
 
 ?>
