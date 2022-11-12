43e2f06f0044236924bd83777e34b2b3ff2d59f5
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-12 15:20:02 +0100

    update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo '<h1>PHP Form</h1>';
 echo 'Name: <input type="text">';
 echo '<p>Age: <input type="text"></p>';
 echo '<p>Email: <input type="text"></p>';
-echo '<p>Gender: <input type="radio", value="Female"> <input type="radio", value="Male"></p>';
+echo '<p>Gender: <input type="radio", value="female">Female <input type="radio", value="male">Male</p>';
 echo '<p><input type="button", value="submit"></p>';
 
 ?>
