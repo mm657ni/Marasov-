fc8e341539abf6db80747ac63577bc3480f57a3d
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-12 13:36:07 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 echo '<h1>PHP Form</h1>'; 
-<?php echo (int)$_POST['age']; ?>
+
+<p>Your name: <input type="text" name="name" /></p>
+<p>Your age: <input type="text" name="age" /></p>
+<p><input type="submit" /></p>
 
 ?>
