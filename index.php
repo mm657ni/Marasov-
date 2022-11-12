824c663b30edbb497a534526d5415e7d65102b98
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-12 13:48:32 +0100

    update index.php

--- index.php
+++ index.php
@@ -2,10 +2,4 @@
 
 echo '<h1>PHP Form</h1>'; 
 
-<form action="getParameters.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
 ?>
