bc5929e6a7a9256c10475034cfb49a7d926be79d
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-18 09:48:28 +0100

    update index.php

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ echo 'Name: <input type="text">';
 echo '<p>Age: <input type="text"></p>';
 echo '<p>Email: <input type="text"></p>';
 echo '<p>Phone number: <input type="text"></p>';
-echo '<p>Gender: <input type="radio", value="female">Female <input type="radio", value="male">Male</p>';
+echo '<p>Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea></p>';
+echo '<p>Gender: <input type="radio" name ="gender"
+ <?php if (isset($gender) && $gender=="female") echo "checked";?> 
+ value="female">Female 
+ <input type="radio" name="gender"
+ <?php if (isset($gender) && $gender=="male") echo "checked";?>
+ value="male">Male</p>';
 
 
 echo '<p><input type="button", value="submit"></p>';
