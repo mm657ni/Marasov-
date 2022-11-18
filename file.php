5f645fc23fc43503bc41114e51b6de993436140e
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-18 14:20:11 +0100

    update file.php

--- file.php
+++ file.php
@@ -1,13 +1,21 @@
 <?php
         echo '<h1>File</h1>';
         
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
+        $name = $_POST["name"];
+        $age = $_POST["age"];
+        $email = $_POST["email"];
+        $num = $_POST["number"];
+        $com = $_POST["comment"];
+        $gen = $_POST["gender"];
         
         $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
-        $text1 = "a=" . $sn1 . " b=" . $sn2;
         
-        fwrite($file1, $text1);
+        fwrite($file1, "Name: ".$name.);
+        fwrite($file1, $age);
+        fwrite($file1, $email);
+        fwrite($file1, $num);
+        fwrite($file1, $com);
+        fwrite($file1, $gen);
         fclose($file1);
         
         $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
@@ -15,8 +23,8 @@
         fwrite($file2, $text2);
         fclose($file2);
         
-        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
-        $text3 = fread($file3,filesize("actuator.txt"));
+        $file3 = fopen("sensors.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("sensors.txt"));
         echo $text3;
         fclose($file3);
     ?>
