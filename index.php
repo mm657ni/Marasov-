6b701a6dd588a0a1165b91f73295a5203bf7d9d0
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-18 15:14:05 +0100

    update index.php

--- index.php
+++ index.php
@@ -14,16 +14,16 @@ echo '<h1>PHP Form</h1>';
             <br><br>
             Phone Number: <input = "text" name = "number" >
             <br><br>
-            Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+            Comment: <textarea name="comment" rows="5" cols="40"></textarea>
             <br><br>
 
             Gender:
             <input type="radio" name="gender"
             <?php if (isset($gender) && $gender=="female") echo "checked";?>    
-            value="female">Female
+            >Female
             <input type="radio" name="gender"
             <?php if (isset($gender) && $gender=="male") echo "checked";?>
-            value="male">Male
+            >Male
             
             <br><br>
             <input type = "submit" name = "submit" value = "Submit">
