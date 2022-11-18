e2392464705c49d456d0f724be4b70f2585ca9c3
mm657ni <114644614+mm657ni@users.noreply.github.com>
2022-11-18 13:46:10 +0100

    update getParameters.php

--- getParameters.php
+++ getParameters.php
@@ -1,12 +1,13 @@
 <?php
         echo '<h1>getParameters page</h1>';
         
+        
         $name = $_GET["name"];
         $age = $_GET["age"];
         $email = $_GET["email"];
         $num = $_GET["number"];
         $com = $_GET["comment"];
-        $gen = $_GET["gender"]
+        $gen = $_GET["gender"];
         
         echo $name;
         echo "<br>";
